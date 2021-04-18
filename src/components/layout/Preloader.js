e39b685a4d80e2757c3d56db736d0d1a6ecb3fb1
Zlatko Marjanovic <zlajaa2000@gmail.com>
2021-04-19 01:08:34 +0200

    Initial commit
    Initial commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Preloader = (props) => {
+return(
+  <div className="progress blue lighten-4">
+    <div className = "indeterminante blue"></div>
+  </div>
+  )
+}
+export default Preloader
